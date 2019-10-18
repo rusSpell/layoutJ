fb11b5d338e73892c9a546190e6bc6043ce3e953
rusSpell <48530482+rusSpell@users.noreply.github.com>
2019-10-18 19:44:35 +0300

    add bottom

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,13 @@
-$(function(){
-
+(function () {
+    const header = document.querySelector('.header');
+    window.onscroll = () => {
+        if (window.pageYOffset > 50) {
+            header.classList.add('header_active');
+        } else{
+            header.classList.remove('header_active')
+        }
+    };
 
 
   
-});
+}());
