87c1b0bca03e2877dc6da712950e20e200dd5a5a
rusSpell <48530482+rusSpell@users.noreply.github.com>
2019-11-01 23:30:43 +0300

    mobile

--- app/js/main.js
+++ app/js/main.js
@@ -7,7 +7,18 @@
             header.classList.remove('header_active')
         }
     };
-
-
-  
 }());
+
+(function () {
+    const burgerItem=document.querySelector('.burger');
+    const menu=document.querySelector('.header__nav');
+    const menuCloseItem=document.querySelector('.header__nav-close');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header__nav-active')
+    });
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header__nav-active')
+    });
+
+}());
+
